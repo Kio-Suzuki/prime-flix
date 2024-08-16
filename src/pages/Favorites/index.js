98921f0350faf35c18c1d1ad1ab41f00bd0de929
Akio Suzuki <kiosuzuki1@gmail.com>
2024-08-16 11:36:08 -0300

    feat: remove movie from my list

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -12,9 +12,19 @@ function Favorites() {
         setMovies(JSON.parse(myList) || []);
 
     }, [])
+
+    function removeMovie(id) {
+        let removed = movies.filter((movie) => {
+            return (movie.id !== id)
+        })
+        setMovies(removed);
+        localStorage.setItem('@primeflix', JSON.stringify(removed))
+    }
   return (
     <div className={style.myMovies}>
-      <h1>Favorites</h1>
+      <h1>Favorites Movies</h1>
+
+      {movies.length === 0 && <span>Empty</span>}
       <ul>
         {movies.map((item) => {
             return (
@@ -22,7 +32,7 @@ function Favorites() {
                     <span>{item.title}</span>
                     <div>
                         <Link to={`/movie/${item.id}`}>Details</Link>
-                        <button>Excluir</button>
+                        <button onClick={() => removeMovie(item.id)}>Excluir</button>
                     </div>
                 </li>
             )
